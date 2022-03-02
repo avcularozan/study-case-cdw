80c590cf22783d13b4aa062b0d1f312afd44f38f
avcularozan <ozan.avcular.oa@gmail.com>
2022-03-02 22:37:48 +0300

    datajs added

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const initialData = [
+  {
+    id: '01',
+    name: 'Cennet Koyu Modern House',
+    type: 'Exterior Design',
+    date: '2018'
+  },
+  {
+    id: '02',
+    name: 'SwissOtel Bodrum Beach Room',
+    type: 'Interior Design',
+    date: '2015'
+  },
+  {
+    id: '03',
+    name: 'OXA Gündoğan Houses',
+    type: 'Interior & Exterior Design',
+    date: '2017'
+  },
+  {
+    id: '04',
+    name: 'Cennet Koyu Stone House',
+    type: 'Exterior Design',
+    date: '2018'
+  }
+]
